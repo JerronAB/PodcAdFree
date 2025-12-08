5b3845b05256e12385b1a9d94060d1340f9ff27b
JBOracleServer <jerronaboling@gmail.com>
2025-12-08 23:19:29 +0000

    Modifying cleaner

--- cleaner.sh
+++ cleaner.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-SEARCH_DIR="/path"
-# Preview
+
+#delete empty folders
+find /complete -type d -empty -delete
+find /assets -type d -empty -delete
+
+#delete mp3 files older than 30 days in /complete directory
+SEARCH_DIR="/complete"
 echo "Files that will be deleted:"
-find "$SEARCH_DIR" -type f -mtime +3 -name '*.ADS.mp3*'
-# Delete
-find "$SEARCH_DIR" -type f -mtime +3 -name '*.ADS.mp3*' -delete
-# Sleep for 12 hours
-sleep 43200 
+find "$SEARCH_DIR" -type f -mtime +30 -name '*.mp3*'
+find "$SEARCH_DIR" -type f -mtime +30 -name '*.mp3*' -delete
+
+# Sleep for 4 hours
+sleep 144000
