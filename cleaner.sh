75cd9548d8368406457f68d59bc5b6663e691c08
JerronAB <92005206+JerronAB@users.noreply.github.com>
2026-01-06 14:04:07 -0600

    Fixing cleaner script

--- cleaner.sh
+++ cleaner.sh
@@ -4,11 +4,14 @@
 find /complete -type d -empty -delete
 find /assets -type d -empty -delete
 
+#delete mp3 files older than 30 days in /assets directory
+find /assets -type f -mtime +30 -name '*.mp3' -delete
+
 #delete mp3 files older than 30 days in /complete directory
 SEARCH_DIR="/complete"
 echo "Files that will be deleted:"
 find "$SEARCH_DIR" -type f -mtime +30 -name '*.mp3*'
 find "$SEARCH_DIR" -type f -mtime +30 -name '*.mp3*' -delete
-find "$SEARCH_DIR" -type f -mtime +4 -name '*.ADS.mp3*' -delete
+find "$SEARCH_DIR" -type f -mtime +4 -name '*ADS.mp3*' -delete
 # Sleep for 4 hours
 sleep 144000
