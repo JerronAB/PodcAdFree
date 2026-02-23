af6611c1526b07517a7d21e88f371c992160ee1a
JBOracleServer <jerronaboling@gmail.com>
2026-02-23 22:29:47 +0000

    Delete old source MP3s and upgrade submodule

--- cleaner.sh
+++ cleaner.sh
@@ -10,8 +10,8 @@ find /assets -type f -mtime +30 -name '*.mp3' -delete
 #delete mp3 files older than 30 days in /complete directory
 SEARCH_DIR="/complete"
 echo "Files that will be deleted:"
-find "$SEARCH_DIR" -type f -mtime +30 -name '*.mp3*'
-find "$SEARCH_DIR" -type f -mtime +30 -name '*.mp3*' -delete
-find "$SEARCH_DIR" -type f -mtime +4 -name '*ADS.mp3*' -delete
+find "$SEARCH_DIR" -type f -mtime +30 -name '*.mp3'
+find "$SEARCH_DIR" -type f -mtime +30 -name '*.mp3' -delete
+find "$SEARCH_DIR" -type f -mtime +2 -name '*.ADS.mp3' -delete
 # Sleep for 4 hours
 sleep 144000
